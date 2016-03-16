9216bc4b945f792fc093861e8afc5cf9a5cc3b31
John L. Villalovos <john.l.villalovos@intel.com>
2016-03-16 10:43:57 -0700

    Add uncompress-gz-files.py and re-arange files in ansible
    Add uncompress-gz-files.py which will uncompress any *.gz files in the
    current directory.
    
    Also move files a little bit inside ansible role
    
    Change-Id: I648c2956280a6c0c7051e38453a31675014b277c

---
+++ ansible/roles/devstack-gate/files/bin/uncompress-gz-files.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python -tt
+# vim: ai ts=4 sts=4 et sw=4
+
+# Copyright (c) 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+import subprocess
+import sys
+
+BASE_DIR = os.path.abspath('.')
+
+
+def main():
+    for filename in sorted(os.listdir(BASE_DIR)):
+        full_path = os.path.join(BASE_DIR, filename)
+        if not filename.endswith('.gz'):
+            continue
+        cmd = ['gunzip', full_path]
+        print "Exec: {}".format(' '.join(cmd))
+        subprocess.check_call(cmd)
+
+
+if '__main__' == __name__:
+    sys.exit(main())
+
