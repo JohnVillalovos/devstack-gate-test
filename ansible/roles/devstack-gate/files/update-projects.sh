c5a15d6c0c1288e3d8596336da59d2f4684b05de
John L. Villalovos <john.l.villalovos@intel.com>
2016-05-10 09:53:59 -0700

    Add "Update tempest compute flavor_ref/flavor_ref_alt" patch
    Add the patch:
       Update tempest compute flavor_ref/flavor_ref_alt
       https://review.openstack.org/#/c/314662/
    
    Also re-organize the patches into sections of which project they are
    updating.
    
    Change-Id: I9119f9d9b8f0c43242dc37eb191aa9544f8e18c9

--- ansible/roles/devstack-gate/files/update-projects.sh
+++ ansible/roles/devstack-gate/files/update-projects.sh
@@ -8,6 +8,7 @@ set -o errexit
 xtrace=$(set +o | grep xtrace)
 set -o xtrace
 
+# grenade project patches
 echo "****: Fetching the grenade patch"
 # https://review.openstack.org/#/c/241018/ Support TEMPEST_REGEX environment variable
 (cd /opt/stack/new/grenade; git fetch https://review.openstack.org/openstack-dev/grenade refs/changes/18/241018/4 && git cherry-pick FETCH_HEAD)
@@ -18,6 +19,13 @@ echo "****: Fetching debug grenade patch..."
 (cd /opt/stack/new/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/34/314334/1 && git cherry-pick FETCH_HEAD)
 (cd /opt/stack/old/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/34/314334/1 && git cherry-pick FETCH_HEAD)
 
+echo "***: Allow creating floating ip address with Neutron enabled"
+# https://review.openstack.org/#/c/313600/
+(cd /opt/stack/new/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/00/313600/1 && git cherry-pick FETCH_HEAD)
+(cd /opt/stack/old/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/00/313600/1 && git cherry-pick FETCH_HEAD)
+
+
+# devstack-gate project patches
 echo "***: Fetching the devstack-gate TEMPEST_REGEX patch"
 # https://review.openstack.org/#/c/241044/ Send DEVSTACK_GATE_TEMPEST_REGEX to grenade jobs
 (cd /opt/stack/new/devstack-gate; git fetch https://review.openstack.org/openstack-infra/devstack-gate refs/changes/44/241044/3 && git cherry-pick FETCH_HEAD)
@@ -27,10 +35,8 @@ echo "***: Fetching: Archive Ironic VM nodes console logs for 'old' patch"
 (cd /opt/stack/new/devstack-gate; git fetch https://review.openstack.org/openstack-infra/devstack-gate refs/changes/71/290171/1 && git cherry-pick FETCH_HEAD)
 (cd /home/jenkins/workspace/testing/devstack-gate; git fetch https://review.openstack.org/openstack-infra/devstack-gate refs/changes/71/290171/1 && git cherry-pick FETCH_HEAD)
 
-echo "***: Fetching the Ironic disable cleaning patch"
-# https://review.openstack.org/#/c/309115/
-(cd /opt/stack/old/ironic; git fetch https://git.openstack.org/openstack/ironic refs/changes/15/309115/1 && git cherry-pick FETCH_HEAD)
 
+# devstack project patches
 echo "***: Fix ovs-vsctl executed in worlddump.py failed issue"
 # https://review.openstack.org/#/c/311055/
 (cd /opt/stack/new/devstack; git fetch https://review.openstack.org/openstack-dev/devstack refs/changes/55/311055/2 && git cherry-pick FETCH_HEAD)
@@ -40,15 +46,20 @@ echo "***: Export the 'short_source' function"
 (cd /opt/stack/old/devstack; git fetch https://git.openstack.org/openstack-dev/devstack refs/changes/32/313132/6 && git cherry-pick FETCH_HEAD)
 (cd /opt/stack/new/devstack; git fetch https://git.openstack.org/openstack-dev/devstack refs/changes/32/313132/6 && git cherry-pick FETCH_HEAD)
 
+
+# ironic-python-agent project patches
 echo "***: Stop using git:// and be nice to people behind proxy servers"
 # https://review.openstack.org/#/c/313123/
 (cd /opt/stack/old/ironic-python-agent; git fetch https://git.openstack.org/openstack/ironic-python-agent refs/changes/23/313123/1 && git cherry-pick FETCH_HEAD)
 
-echo "***: Allow creating floating ip address with Neutron enabled"
-# https://review.openstack.org/#/c/313600/
-(cd /opt/stack/new/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/00/313600/1 && git cherry-pick FETCH_HEAD)
-(cd /opt/stack/old/grenade; git fetch https://git.openstack.org/openstack-dev/grenade refs/changes/00/313600/1 && git cherry-pick FETCH_HEAD)
+# ironic project patches
+echo "***: Fetching the Ironic disable cleaning patch"
+# https://review.openstack.org/#/c/309115/
+(cd /opt/stack/old/ironic; git fetch https://git.openstack.org/openstack/ironic refs/changes/15/309115/1 && git cherry-pick FETCH_HEAD)
 
+echo "***: Update tempest compute flavor_ref/flavor_ref_alt"
+# https://review.openstack.org/#/c/314662/
+(cd /opt/stack/old/ironic; git fetch https://git.openstack.org/openstack/ironic refs/changes/62/314662/1 && git cherry-pick FETCH_HEAD)
 
 # Prep the pip cache for the stack user, which is owned by the 'jenkins' user at this point
 if [ -d /opt/git/pip-cache/ ]
